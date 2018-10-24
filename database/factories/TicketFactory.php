785a2669a7c9df1a0168b2481841b312af012b15
jesper0438 <robr0002@hz.nl>
2018-10-24 14:14:56 +0200

    updated the ticketfactory which can be used for testing

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -2,8 +2,13 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Ticket::Class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'description' => $faker->name,
+        'category' => $faker->name,
+        'user' => $faker->name,
+        'device' => $faker->randomDigit,
+        'status_id' => '1',
     ];
 });
